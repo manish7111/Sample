58c8a43e81291b4db297134d06bd8ebf8cf35119
mmkr7111 <mmkr7111@gmail.com>
2020-09-28 14:48:01 +0530

    Added

--- Addition/Program.cs
+++ Addition/Program.cs
@@ -6,11 +6,7 @@ namespace Addition
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Addition Problem \n Enter a, b values");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            AddNumbers add = new AddNumbers();
-            Console.WriteLine("Result is"+" "+add.Sum(a, b));
+            console.WriteLine();
 
         }
     }
